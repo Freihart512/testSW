a8e6709d23399f58e219e40c6877cc585c357858
Carlos Gonzalez <carlosfabian512@gmail.com>
2024-09-27 12:55:24 -0600

    bd

--- src/Postgres/shared.ts
+++ src/Postgres/shared.ts
@@ -1,24 +1,11 @@
-export enum RowStatus {
-  ACTIVE = "pending",
-  DELETE = "completed",
-}
-
-
-import { CreateDateColumn, UpdateDateColumn, Column } from "typeorm";
+import { CreateDateColumn, UpdateDateColumn, Column, DeleteDateColumn } from "typeorm";
 
 export abstract class BaseEntity {
   @CreateDateColumn({ update: false })
   created_at: Date;
 
-  @UpdateDateColumn()
-  updated_at: Date;
-
-  @Column({
-    type: "enum",
-    enum: RowStatus,
-    default: RowStatus.ACTIVE,
-  })
-  register_status: RowStatus;
+  @DeleteDateColumn()
+  deletedAt?: Date;
 }
 
 
